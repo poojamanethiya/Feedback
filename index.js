7b1e524e8cdbb297c23f986ebd164f0b0da3d529
pooja manethiya <50955432+poojamanethiya@users.noreply.github.com>
2021-03-17 18:17:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,24 +16,35 @@ else
 var n= nam.trim()+'    '+mname.trim()+'   '+lname.trim();
 }
 
-var result = document.getElementById("a").innerText ;
-var gender = document.querySelector('input[name=gender]:checked').value;
-result = result + gender;
-result = result+ n +"     "+"Says : "+ '\n'+ sug + '\n'+ Date();
-
-var x , txt ="";
-var arr = {result};
-for(x in arr)
+if(nam == "")
 {
-txt+=arr[x] +'\n\n';
-
-document.getElementById("a").innerText = txt ;
-
-
+    alert("ENTER YOUR FIRST NAME!!");
+}
+else if(lname == "")
+{
+    alert("ENTER YOUR LAST NAME!!");
+}
+else if(sug == "")
+{
+    alert("PLEASE ENTER YOUR SUGGESTION!!");
 }
 
 
+else{
+    var result = document.getElementById("a").innerText ;
+    
+    var gender = document.querySelector('input[name=gender]:checked').value;
+ 
+  result = result + gender;
+    result = result+ n +"     "+"Says : "+ '\n'+ sug + '\n'+ Date();
+    
+    var x =document.createElement("p");
+    x.innerText = result;
+    document.getElementById("t").appendChild(x);
+    
 
+    
+}
 
 }
 function resetf()
